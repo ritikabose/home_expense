3ad1bf30d504d735f229a40d32d4c8e31ed4ac67
ritikabose <r3majumdar@yahoo.com>
2011-06-06 21:46:07 -0400

    Inital commit

---
+++ README.markdown
@@ -0,0 +1,8 @@
+# Ruby on Rails Sample Application For Home Expense
+
+This is the sample application for
+[*Home expense*](http://railstutorial.org/)
+by [Ritika Bose].
+
+In this application you can manage your monhly home
+expense Online.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeExpense::Application.config.secret_token = '3996fa7bd30264a8284b5b613555fce9269a783c223e28d310e38b786ebee6edf50c8e87d52092a7edb2ee654217630c8c7c1c2755431dd5f0b3bf19f5c51b90'
